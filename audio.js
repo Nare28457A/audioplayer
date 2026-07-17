3a5370cea04f7957de8cf2567bf37b92bdb77c45
Nare28457A <nare.alaverdyan@tumo.org>
2026-07-17 16:17:26 +0400

    final

--- audio.js
+++ audio.js
@@ -1,5 +1,5 @@
 let data = {
-    title : [
+    title: [
         "Aram Asatryan - Es qez shat shat shat sirum em",
         "311_-_However_far_away_I_will_always_love_you",
         "Aram Asatryan  - Anna",
@@ -9,12 +9,12 @@ let data = {
         "Aram Asatryan - Srjaran",
         "Ministry_Of_Magic_-_However_Far_Away_",
         "Niyaz_-_In_The_Shadow_Of_Life_",
-        
+
 
 
     ],
 
-    song : [
+    song: [
         "music/ Aram Asatryan  - Es qez shat shat shat sirum em.mp3",
         "music/311_-_However_far_away_I_will_always_love_you_(mp3.pm).mp3",
         "music/Aram Asatryan  - Anna.mp3",
@@ -27,50 +27,50 @@ let data = {
     ],
 
     poster: [
-    "https://cdn-images.dzcdn.net/images/cover/8e48c33cef255ff04f012e469a8004ba/0x1900-000000-80-0-0.jpg",
-    "https://i.ytimg.com/vi/PGmHBwgO3jA/maxresdefault.jpg",
-    "https://music.gisher.me/public/covers/149/273.webp",
-    "https://music.gisher.me/public/covers/149/271.webp",
-    "https://i.ytimg.com/vi/ZdtYRGVg-d0/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AG-B4AC0AWKAgwIABABGH8gSSgqMA8=&rs=AOn4CLAUokF5DWUMox6mK-qD_Aq3YI-8eg",
-    "https://i.ytimg.com/vi/G_ddlPwYH5I/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDNQu9FKzlz3iG6V6WaAd2R5C1PGg",
-    "https://music.gisher.me/public/covers/149/285.webp",
-    "https://i.scdn.co/image/ab67616d0000b273e44109770565bd58df255039",
-    "https://i.scdn.co/image/ab67616d0000b2738fd31abb6f008f04b922c668",
+        "https://cdn-images.dzcdn.net/images/cover/8e48c33cef255ff04f012e469a8004ba/0x1900-000000-80-0-0.jpg",
+        "https://i.ytimg.com/vi/PGmHBwgO3jA/maxresdefault.jpg",
+        "https://music.gisher.me/public/covers/149/273.webp",
+        "https://music.gisher.me/public/covers/149/271.webp",
+        "https://i.ytimg.com/vi/ZdtYRGVg-d0/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AG-B4AC0AWKAgwIABABGH8gSSgqMA8=&rs=AOn4CLAUokF5DWUMox6mK-qD_Aq3YI-8eg",
+        "https://i.ytimg.com/vi/G_ddlPwYH5I/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDNQu9FKzlz3iG6V6WaAd2R5C1PGg",
+        "https://music.gisher.me/public/covers/149/285.webp",
+        "https://i.scdn.co/image/ab67616d0000b273e44109770565bd58df255039",
+        "https://i.scdn.co/image/ab67616d0000b2738fd31abb6f008f04b922c668",
     ]
 }
 
 let song = new Audio()
 let currentSong = 0
 
-window.onload = function (){
-       playSong()
+window.onload = function () {
+    playSong()
 
 }
 
+function playSong() {
+    song.src = data.song[currentSong]
+    let songTitle = document.getElementById("songTitle")
+    
+    songTitle.textContent = data.title[currentSong]
+    
+    let img = document.getElementsByClassName("row1")
+    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
+    console.log(img[0]);
+    
+    let main = document.getElementsByClassName("main")
+    main[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
+    song.play()
+    }
 
-function playSong(){
-           song.src = data.song[currentSong]
-           let songTitle = document.getElementById("songTitle")
-           songTitle.textContent = data.title[currentSong]
 
-           let img = document.getElementById("row1")
-           img.style.backgroundImage = "url ('" + data.poster[currentSong] + "') "
-
-           let main = document.getElementById("main")
-           main.style.backgroundImage =  "url ('" + data.poster[currentSong] + "') "
-           song.play()
-
-}
-
-
-function playOrPauseSong(){
+function playOrPauseSong() {
     let play = document.getElementById("play")
 
-    if(song.paused){
+    if (song.paused) {
         song.play()
         play.src = "images/pause.png"
 
-    }else{
+    } else {
         song.pause()
         play.src = "images/play-button-arrowhead.png"
     }
@@ -78,5 +78,85 @@ function playOrPauseSong(){
 }
 
 
+song.addEventListener("timeupdate", function () {
+    let fill = document.getElementById("fill")
+    let position = song.currentTime / song.duration
+    fill.style.width = position * 100 + "%"
+    convertTime(song.currentTime)
+    totalTime(Math.round(song.duration))
+
+    if (song.ended) {
+        next()
+    }
+})
 
 
+
+function convertTime(seconds) {
+    let currentTime = document.getElementById("currrentTime")
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+    currentTime.textContent = min + " : " + sec
+}
+
+function totalTime(seconds) {
+    let currentTime = document.getElementById("currrentTime")
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+    currentTime.textContent += " / " + min + " : " + sec
+}
+
+function next() {
+    currentSong++;
+    if (currentSong > data.song.length) {
+        currentSong = 0;
+    }
+    playSong();
+    play.src = "images/pause.png"
+}
+
+
+function pre() {
+    currentSong--;
+    if (currentSong < 0) {
+        currentSong = data.song.length - 1;
+    }
+    playSong();
+    play.src = "images/pause.png"
+}
+
+function mute() {
+    let mute = document.getElementById("mute")
+    if (song.muted) {
+        song.muted = false
+        mute.src = "images/volume.png"
+    } else {
+        song.muted = true
+        mute.src = "images/volume-mute.png"
+
+    }
+}
+
+function decrease (){
+    let mute = document.getElementById("mute")
+      song.volume -= 0.2
+
+      if (song.volume <= 0.1) {
+        mute.src = "images/volume-mute.png"
+      }
+    }
+
+      function increase (){
+        let mute = document.getElementById("mute")
+        song.volume += 0.2
+ 
+      if (song.volume >= 0.2) {
+          mute.src = "images/volume.png"
+      }
+}
